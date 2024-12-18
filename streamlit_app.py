2652f302f6b49640565151a1367e9277ff6cffb0
yamaguchi7777 <answerthesnow@au.com>
2024-12-18 08:00:51 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Streamlitライブラリをインポート
 import streamlit as st
+import random
 
 # ページ設定（タブに表示されるタイトル、表示幅）
 st.set_page_config(page_title="タイトル", layout="wide")
@@ -7,3 +8,7 @@ st.set_page_config(page_title="タイトル", layout="wide")
 # タイトルを設定
 st.title('おみくじアプリ')
 
+omikuji=["大吉","中吉","小吉","吉","凶","大凶","末吉"]
+
+if st.button("おみくじをひく"):
+    a=random.choice(omikuji)
